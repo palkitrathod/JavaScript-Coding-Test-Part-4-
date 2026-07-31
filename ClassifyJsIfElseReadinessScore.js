33228f867ec9bde27578e4d46041df78bff6e497
Palkit Rathod <135707401+palkitrathod@users.noreply.github.com>
2026-07-31 07:17:02 +0530

    Implement classifyJsIfElseScore function

---
+++ ClassifyJsIfElseReadinessScore.js
@@ -0,0 +1,23 @@
+function classifyJsIfElseScore(score)
+{
+  console.log(score)
+
+  if (score >= 90)
+  {
+    return "EXCELLENT"
+  }
+  else if (score >= 75 && score <= 89)
+  {
+    return "GOOD"
+  }
+  else if (score >= 50 && score <= 74)
+  {
+    return "NEEDS_PRACTICE"
+  }
+  else (score < 50)
+  {
+    return "REVISIT"
+}
+
+    
+}
